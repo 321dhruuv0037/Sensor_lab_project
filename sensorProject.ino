45f272bcddbf96dc6c0373457690defa22af95dd
321dhruuv0037 <321dhruuv0037@dbit.in>
2024-03-28 15:49:01 +0530

    Added buzzer sound

--- sensorProject.ino
+++ sensorProject.ino
@@ -1,9 +1,21 @@
 // Define pins and variable for input sensor and output led and buzzer
-const int mq135_aqi_sensor = A1;
+int mq135_aqi_sensor = A1;
 int flame_sensor_pin = 4;    
-const int red_led = 2;
+int red_led = 2;
 int led_pin = 10;                // initializing the pin 2 as the led pin
- 
+int buzzer_pin = 6;
+const int NOTE_C4 = 262;
+const int NOTE_E4 = 330;
+const int NOTE_G4 = 392;
+const int NOTE_C5 = 523;
+const int NOTE_B4 = 494;
+const int NOTE_A4 = 440;
+const int NOTE_F4 = 349;
+const int NOTE_E5 = 659;
+const int NOTE_D5 = 587;
+const int NOTE_D4 = 294;
+
+
 int flame_pin = HIGH;    
 // Set threshold for AQI
 int aqi_ppm = 0;
@@ -14,6 +26,7 @@ void setup() {
   pinMode (red_led, OUTPUT);
   pinMode(flame_sensor_pin, INPUT);     // declaring sensor pin as input pin for Arduino
   pinMode(led_pin, OUTPUT);             // declaring led pin as output pin
+  pinMode(buzzer_pin, OUTPUT);          // declaring buzzer pin as output pin
 
   digitalWrite(red_led, LOW);
 
@@ -59,13 +72,67 @@ void loop() {
   if (flame_pin == LOW)                               // checks whether there is flame, as flame present = sensor output low
   {
     Serial.println("FLAME, FLAME, FLAME");
-    digitalWrite(led_pin, HIGH);                                      // play a melody on the buzzer
+    digitalWrite(led_pin, HIGH);                                     
+    playMelody();                                     // play a melody on the buzzer
   }
   else
   {
     Serial.println("no flame");
-    digitalWrite(led_pin, LOW);                              // stop playing any tone
+    digitalWrite(led_pin, LOW);                              
+    noTone(buzzer_pin);                               // stop playing any tone
   }
   
   delay (700);
+}
+void playMelody()
+{
+  // tone(buzzer_pin, NOTE_F4, 200); // C4
+  // delay(200);
+  // tone(buzzer_pin, NOTE_C5, 200); // E4
+  // delay(200);
+  // tone(buzzer_pin, NOTE_A4, 200); // G4
+  // delay(200);
+  // tone(buzzer_pin, NOTE_C5, 200); // C5 (double duration)
+  // delay(200);
+  // tone(buzzer_pin, NOTE_G4, 200); // B4
+  // delay(200);
+  // tone(buzzer_pin, NOTE_A4, 200); // A4
+  // delay(200);
+  // tone(buzzer_pin, NOTE_G4, 200); // G4 (double duration)
+  // delay(200);
+  // tone(buzzer_pin, NOTE_F4, 400); // F4
+  // delay(400);
+  // tone(buzzer_pin, NOTE_F4, 200); // C4
+  // delay(200);
+  // tone(buzzer_pin, NOTE_C5, 200); // E4
+  // delay(200);
+  // tone(buzzer_pin, NOTE_A4, 200); // G4
+  // delay(200);
+  // tone(buzzer_pin, NOTE_C5, 200); // C5 (double duration)
+  // delay(200);
+  // tone(buzzer_pin, NOTE_G4, 200); // B4
+  // delay(200);
+  // tone(buzzer_pin, NOTE_A4, 200); // A4
+  // delay(200);
+  // tone(buzzer_pin, NOTE_G4, 200); // G4 (double duration)
+  // delay(200);
+  // tone(buzzer_pin, NOTE_F4, 400); // F4
+  // delay(400);
+  tone(buzzer_pin, NOTE_C4, 200); // C4
+  delay(200);
+  tone(buzzer_pin, NOTE_D4, 200); // D4
+  delay(200);
+  tone(buzzer_pin, NOTE_E4, 400); // E4 (double duration)
+  delay(400);
+  tone(buzzer_pin, NOTE_C4, 200); // C4
+  delay(200);
+  tone(buzzer_pin, NOTE_D4, 200); // D4
+  delay(200);
+  tone(buzzer_pin, NOTE_E4, 400); // E4 (double duration)
+  delay(400);
+  tone(buzzer_pin, NOTE_D4, 200); // D4
+  delay(200);
+  tone(buzzer_pin, NOTE_C4, 400); // C4 (double duration)
+  delay(400);
+  
 }
